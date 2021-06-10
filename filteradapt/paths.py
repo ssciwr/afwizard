c043912c850ad05f9e518a30f16890d7fbe9a1a0
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2021-06-10 17:20:41 +0200

    Add nbval testing for notebooks

--- filteradapt/paths.py
+++ filteradapt/paths.py
@@ -14,16 +14,23 @@ def locate_file(filename):
     if os.path.isabs(filename):
         return filename
 
+    # Try relative path w.r.t. the current working directory
     local = os.path.join(os.getcwd(), filename)
     if os.path.exists(local):
         return local
 
+    # Try relative path w.r.t. the Python package base directory
+    package = os.path.join(os.path.split(__file__)[0], "..", filename)
+    if os.path.exists(package):
+        return package
+
+    # Try relative path w.r.t. the specified data directory
     indata = os.path.join(_data_dir, filename)
     if os.path.exists(indata):
         return indata
 
     raise FileNotFoundError(
-        f"Cannot locate file {filename}, maybe use set_data_directory to point to the correct location"
+        f"Cannot locate file {filename}, maybe use set_data_directory to point to the correct location."
     )
 
 
--- tests/test_filteradapt.py
+++ tests/test_filteradapt.py
@@ -2,4 +2,9 @@ import filteradapt
 
 
 def test_filteradapt():
+    # Load a dataset
     dataset = filteradapt.DataSet("data/500k_NZ20_Westport.laz")
+
+    # Test visualization - this is not actually very good in the absence of a display
+    # But it helps in measuring coverage of the test suite.
+    dataset.show()
