5288980e27e8f65a172a57019f871db6fa046fa0
Gwydion <gwy.daskalakis@googlemail.com>
2021-06-17 15:46:28 +0200

    working user api

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -17,7 +17,7 @@ import sys
 #
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath("../../filteradapt"))
+sys.path.insert(0, os.path.abspath("../.."))
 print(sys.path)
 # -- Project information -----------------------------------------------------
 
