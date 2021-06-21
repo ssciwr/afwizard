380630fb10b9ffe3e5448a4cf2e21edbb4c745c2
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2021-06-21 13:55:38 +0200

    Document the necessity for __init__.__all__

--- filteradapt/__init__.py
+++ filteradapt/__init__.py
@@ -1,4 +1,8 @@
-__all__ = ["DataSet", "set_data_directory"]
-
+# Import those functions and objects that we consider the package user API
 from filteradapt.dataset import DataSet
 from filteradapt.paths import set_data_directory
+
+# This is necessary for autodoc to generate the User API
+# The order of objects in this list defines the order in
+# the Sphinx documentation.
+__all__ = ["DataSet", "set_data_directory"]
