a84261e46f0abd8c7ca6d49a59542f6e4a9da799
Gwydion <gwy.daskalakis@googlemail.com>
2021-06-28 13:08:07 +0200

    added better angle

--- adaptivefiltering/visualization.py
+++ adaptivefiltering/visualization.py
@@ -1,7 +1,7 @@
 import ipyvolume.pylab as vis
 
 
-def vis_pointcloud(x, y, z, az=0, el=0):
+def vis_pointcloud(x, y, z):
     """Visualization of a point cloud in Jupyter notebooks
 
     :param x: The array x-coordinates of the point cloud
@@ -12,9 +12,15 @@ def vis_pointcloud(x, y, z, az=0, el=0):
     :type z: numpy.array
     """
     fig = vis.figure(width=1000)
-    vis.scatter(x, y, z, color="red", size=0.05)
+    vis.scatter(
+        x,
+        y,
+        z,
+        color="red",
+        size=0.05,
+    )
     vis.style.box_off()
-    vis.view(azimuth=az, elevation=el)
+    vis.view(azimuth=180, elevation=90)
     fig.xlim = (min(x), max(x))
     fig.ylim = (min(y), max(y))
     fig.zlim = (min(z), max(z))
