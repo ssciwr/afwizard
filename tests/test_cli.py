5a4da07c45a1aa4e210e87a6e6c71b4bda48f936
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2021-07-22 09:20:21 +0200

    Do not duplicate the list of implemented OPALS modules in tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,12 +1,14 @@
 from adaptivefiltering.opals import _automated_opals_schema, opals_is_present
 
+from .test_opals import _availableOpalsModules
+
 from click.testing import CliRunner
 import json
 import pytest
 
 
 @pytest.mark.skipif(not opals_is_present(), reason="OPALS not found.")
-@pytest.mark.parametrize("mod", ["Grid", "Cell", "RobFilter"])
+@pytest.mark.parametrize("mod", _availableOpalsModules)
 def test_opals_schema_cli(mod):
     runner = CliRunner()
     result = runner.invoke(_automated_opals_schema, mod)
