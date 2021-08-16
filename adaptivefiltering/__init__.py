c88ec100a93b01e69bbe8802cac6acaedac2d5d2
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2021-08-16 12:33:19 +0200

    Introduce an ASPRS classification object with slicing notation

--- adaptivefiltering/__init__.py
+++ adaptivefiltering/__init__.py
@@ -4,7 +4,7 @@ import adaptivefiltering.pdal
 
 # Import those functions and objects that we consider the package user API
 from adaptivefiltering.apps import pipeline_tuning
-from adaptivefiltering.dataset import DataSet
+from adaptivefiltering.dataset import DataSet, asprs
 from adaptivefiltering.filter import load_filter, save_filter
 from adaptivefiltering.opals import set_opals_directory
 from adaptivefiltering.paths import set_data_directory
@@ -13,6 +13,7 @@ from adaptivefiltering.paths import set_data_directory
 # The order of objects in this list defines the order in
 # the Sphinx documentation.
 __all__ = [
+    "asprs",
     "DataSet",
     "load_filter",
     "pipeline_tuning",
--- adaptivefiltering/dataset.py
+++ adaptivefiltering/dataset.py
@@ -1,6 +1,7 @@
 from adaptivefiltering.paths import locate_file, get_temporary_filename
 from adaptivefiltering.utils import AdaptiveFilteringError
 
+import functools
 import os
 import shutil
 import sys
@@ -168,3 +169,85 @@ class DataSet:
     def convert(cls, dataset):
         """Convert this dataset to an instance of DataSet"""
         return dataset.save(get_temporary_filename(extension="las"))
+
+
+class ASPRSClassification:
+    """A data structure that describes the ASPRS Standard Lidar Point Classes"""
+
+    @functools.lru_cache
+    def _mapping(cls):
+        """Mapping of string names to classification values.
+
+        This only exposes the subset relevant to our use cases
+        """
+        return {
+            "unclassified": 1,
+            "ground": 2,
+            "low_vegetation": 3,
+            "medium_vegetation": 4,
+            "high_vegetation": 5,
+            "building": 6,
+            "low_point": 7,
+            "water": 9,
+            "road_surface": 11,
+        }
+
+    def __getitem__(self, class_):
+        """Create a classification value sequence from input"""
+
+        # This might be a slice, which we will resolve to an integer sequence
+        if isinstance(class_, slice):
+            # If start is not given, it is zero
+            start = class_.start
+            if start is None:
+                start = 0
+
+            # If stop is not given, it is the maximum possible classification value: 255
+            stop = class_.stop
+            if stop is None:
+                stop = 255
+            # This adaptation is necessary to be able to use the range generator below
+            stop = stop + 1
+
+            # Collect the list of arguments to the range generator
+            args = [start, stop]
+
+            # Add a third parameter iff the slice step parameter was given
+            if class_.step is not None:
+                args.append(class_.step)
+
+            # Return the tuple of classification values
+            return tuple(range(*args))
+
+        def _process_list_item(item):
+            """Process a single given classification value"""
+            if isinstance(item, str):
+                # If this is a string we try to split it at commas
+                subitems = item.split(",")
+                if len(subitems) > 1:
+                    # If a comma-separated list was given, we return the union of classification
+                    # values for each of the entries of that list
+                    return sum((_process_list_item(i.strip()) for i in subitems), ())
+                else:
+                    try:
+                        # For strings, we do a lookup in our internal name mapping
+                        return (self._mapping()[item.strip()],)
+                    except KeyError:
+                        raise AdaptiveFilteringError("Classifier not known")
+
+            # If no string was given, we assume that a plain integer was given
+            assert isinstance(item, int)
+            return (item,)
+
+        def _sort_uniq(items):
+            return tuple(sorted(set(items)))
+
+        # If a tuple of items was given, we return the union of classification values
+        if isinstance(class_, tuple):
+            return _sort_uniq(sum((_process_list_item(i) for i in class_), ()))
+        else:
+            return _sort_uniq(_process_list_item(class_))
+
+
+# A global object of type ASPRSClassification is available for simple use
+asprs = ASPRSClassification()
