4857ba7c8560d70613058d343eefce44d242d5eb
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2021-08-16 13:05:04 +0200

    Move ASPRS implementation to its own module

--- adaptivefiltering/__init__.py
+++ adaptivefiltering/__init__.py
@@ -4,7 +4,8 @@ import adaptivefiltering.pdal
 
 # Import those functions and objects that we consider the package user API
 from adaptivefiltering.apps import pipeline_tuning
-from adaptivefiltering.dataset import DataSet, asprs
+from adaptivefiltering.asprs import asprs
+from adaptivefiltering.dataset import DataSet
 from adaptivefiltering.filter import load_filter, save_filter
 from adaptivefiltering.opals import set_opals_directory
 from adaptivefiltering.paths import set_data_directory
