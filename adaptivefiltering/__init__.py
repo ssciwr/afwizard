677cee95b76858c3300c1e770273b5609791c53a
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2021-10-21 13:41:53 +0200

    Export reproject_dataset to the user API

--- adaptivefiltering/__init__.py
+++ adaptivefiltering/__init__.py
@@ -6,7 +6,7 @@ import adaptivefiltering.pdal
 # Import those functions and objects that we consider the package user API
 from adaptivefiltering.apps import pipeline_tuning
 from adaptivefiltering.asprs import asprs
-from adaptivefiltering.dataset import DataSet, remove_classification
+from adaptivefiltering.dataset import DataSet, remove_classification, reproject_dataset
 from adaptivefiltering.filter import load_filter, save_filter
 from adaptivefiltering.lastools import set_lastools_directory
 from adaptivefiltering.opals import set_opals_directory
@@ -20,6 +20,7 @@ __all__ = [
     "DataSet",
     "pipeline_tuning",
     "remove_classification",
+    "reproject_dataset",
     "load_filter",
     "save_filter",
     "set_data_directory",
