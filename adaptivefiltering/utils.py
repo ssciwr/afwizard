7da41efb8f7de41b4f0480cf11b4c865d74df6c5
GwydionJon <gwy.daskalakis@googlemail.com>
2021-11-19 13:29:02 +0100

    added crop function for pictures

--- adaptivefiltering/utils.py
+++ adaptivefiltering/utils.py
@@ -1,4 +1,6 @@
 import collections
+from os import pipe
+from PIL import Image, ImageChops
 
 
 class AdaptiveFilteringError(Exception):
@@ -15,3 +17,14 @@ def stringify_value(value):
         return " ".join(value)
 
     return str(value)
+
+
+def trim(path):
+    im = Image.open(path)
+    bg = Image.new(im.mode, im.size, im.getpixel((0, 0)))
+    diff = ImageChops.difference(im, bg)
+    diff = ImageChops.add(diff, diff, 2.0, -100)
+    bbox = diff.getbbox()
+    if bbox:
+        im = im.crop(bbox)
+        im.save(path)
