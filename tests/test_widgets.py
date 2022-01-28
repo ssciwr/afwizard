fca74f8566340039bc2d95156c27f5a341764814
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2022-01-28 16:07:22 +0100

    Use upstream version of WidgetForm

--- tests/test_widgets.py
+++ tests/test_widgets.py
@@ -27,28 +27,6 @@ _example_schema = [
 ]
 
 
-@pytest.mark.parametrize("schema", _example_schema)
-def test_widget_form(schema):
-    widget = WidgetForm(schema)
-    widget.show()
-    jsonschema.validate(instance=pyrsistent.thaw(widget.data), schema=schema)
-
-    # Get data, set it, get it again and compare the resulting document
-    data = widget.data
-    widget.data = data
-    data2 = widget.data
-    assert data == data2
-
-
-def test_pattern_rule():
-    schema = {"type": "string", "pattern": "[^_-]*"}
-    widget = WidgetForm(schema)
-    widget.data = "blawithoutdashor underscore"
-
-    with pytest.raises(WidgetFormError):
-        widget.data = "ba_ad"
-
-
 def test_widget_form_with_labels_pattern():
     schema = {
         "items": {"pattern": "[a-z\\-]*", "type": "string"},
