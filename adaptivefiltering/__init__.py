154b2674055b35ebe2a64957d9ffa9d93d7f3b0e
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2022-03-04 16:02:02 +0100

    Bump version

--- adaptivefiltering/__init__.py
+++ adaptivefiltering/__init__.py
@@ -1,5 +1,5 @@
 # This is the single source of truth
-__version__ = "1.0.0a1"
+__version__ = "1.0.0b0"
 
 # Make sure to import modules that register filter backends
 import adaptivefiltering.lastools
