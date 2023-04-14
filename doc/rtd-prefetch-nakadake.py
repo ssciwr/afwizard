caf5e260c8e0ff86164d18284da3d0c106e1efb7
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2023-04-14 16:10:56 +0200

    Also prefetch test data

--- doc/rtd-prefetch-nakadake.py
+++ doc/rtd-prefetch-nakadake.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
-from afwizard.paths import nakadake_data
+from afwizard.paths import download_test_file, nakadake_data
 
 # Pre-fetch all the Nakadake data
 for filename in nakadake_data.registry:
     nakadake_data.fetch(filename)
+
+# Also pre-fetch testing data
+download_test_file("blubb")
--- doc/source/conf.py
+++ doc/source/conf.py
@@ -5,7 +5,6 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 import os
-import subprocess
 import sys
 
 # -- Path setup --------------------------------------------------------------
