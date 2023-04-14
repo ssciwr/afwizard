d59112273411e45567a503724c3c5e3a2f52145e
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2023-04-14 13:05:07 +0200

    Fix path concatenation

--- doc/rtd-write-ssh.py
+++ doc/rtd-write-ssh.py
@@ -2,7 +2,7 @@
 
 import os
 
-with open(os.path.expanduser("~") / ".ssh/id_ed25519") as f:
+with open(os.path.join(os.path.expanduser("~"), ".ssh/id_ed25519")) as f:
     # We are running into https://github.com/readthedocs/readthedocs.org/issues/8636. Therefore,
     # we absolutely need to elimiate the single quotes.  
     f.write(os.environ("SSH_KEY").strip("'"))
