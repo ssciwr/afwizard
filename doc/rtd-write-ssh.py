ceaf4c8284cad49f95b503bac17119de81b9de54
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2023-04-14 13:14:18 +0200

    dict lookup

--- doc/rtd-write-ssh.py
+++ doc/rtd-write-ssh.py
@@ -5,4 +5,4 @@ import os
 with open(os.path.join(os.path.expanduser("~"), ".ssh/id_ed25519"), "w") as f:
     # We are running into https://github.com/readthedocs/readthedocs.org/issues/8636. Therefore,
     # we absolutely need to elimiate the single quotes.  
-    f.write(os.environ("SSH_KEY").strip("'"))
+    f.write(os.environ["SSH_KEY"].strip("'"))
